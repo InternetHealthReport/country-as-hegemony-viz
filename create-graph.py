0ccf9d907a377e1a83547878876c96528b4780ba
Emile Aben <emileaben@gmail.com>
2022-03-28 16:48:10 +0200

    different versions of country-hege

--- create-graph.py
+++ create-graph.py
@@ -57,10 +57,11 @@ with open("input-asns.txt") as inf:
                     if v > thres: ### only if over X pct of pop
                         eyeball_weights[ str( k ) ] = v # stringify keys
         elif line.startswith('{'): # different version of the hege code
-            temp_e = ast.literal_eval( line )
-            for k,v in temp_e.iteritems():
-                if v > thres: ### only if over X pct of pop
-                    eyeball_weights[ str( k ) ] = v # stringify keys
+            continue
+            #temp_e = ast.literal_eval( line )
+            #for k,v in temp_e.iteritems():
+            #    if v > thres: ### only if over X pct of pop
+            #        eyeball_weights[ str( k ) ] = v # stringify keys
         else:
             line = line.rstrip('\n')
             f = line.split(', ')
